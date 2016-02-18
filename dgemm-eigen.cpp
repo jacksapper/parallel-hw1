9c6a10ec010b0b7a0a21c6d6a7474e0000f3c995
Jason Hatton <jason.a.hatton@gmail.com>
2016-02-18 14:17:25 -0600

    Added Eigen Experiment

---
+++ dgemm-eigen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+const char* dgemm_desc = "Breaking da rules, Breaking da rules";
+
+void square_dgemm (int n, double* A, double* B, double* C)
+{
+	MatrixXd Ayy = Map<MatrixXd>(A,n,n);
+	MatrixXd Bee = Map<MatrixXd>(B,n,n);
+	MatrixXd Cee = Map<MatrixXd>(C,n,n);
+	Cee += (Ayy*Bee);
+}
